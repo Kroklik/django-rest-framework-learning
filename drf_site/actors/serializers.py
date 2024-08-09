93cdad5ab52a389e344342c0cf52c74305b6ba8a
Kroklik <pupzemli092@gmail.com>
2024-08-09 11:42:58 +0500

    'serializers'

--- drf_site/actors/serializers.py
+++ drf_site/actors/serializers.py
@@ -1,8 +1,38 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+
 from .models import Actor
+from rest_framework.renderers import JSONRenderer
 
 
-class ActorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Actor
-        fields = ('title', 'category_id')
+# class ActorModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class ActorSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField(max_length=512)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = ActorModel(title='Pipidastr', content='sigma')
+#     model_sr = ActorSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title": "Terminator", "content": "Content: sigma"}')
+#     data = JSONParser().parse(stream)
+#     serializer = ActorSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- drf_site/actors/views.py
+++ drf_site/actors/views.py
@@ -1,10 +1,44 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Actor
 from .serializers import ActorSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
 
-class ActorApiView(generics.ListAPIView):
-    queryset = Actor.objects.all()
-    serializer_class = ActorSerializer
+# class ActorApiView(generics.ListAPIView):
+#     queryset = Actor.objects.all()
+#     serializer_class = ActorSerializer
+
+# class ActorApiView(APIView):
+#     def get(self, request):
+#         lst = Actor.objects.all()
+#         return Response({'posts': ActorSerializer(lst, many=True).data})
+#
+#     def post(self, request):
+#         post_new = Actor.objects.create(
+#             title=request.data['title'],
+#             content=request.data['content'],
+#             category_id=request.data['category_id']
+#         )
+#         return Response({'new_post': ActorSerializer(post_new).data})
+
+class ActorApiView(APIView):
+    def get(self, request):
+        lst = Actor.objects.all()
+        return Response({'posts': ActorSerializer(lst, many=True).data})
+
+    def post(self, request):
+        serializer = ActorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if serializer.is_valid():
+            post_new = Actor.objects.create(
+                title=serializer.validated_data['title'],
+                content=serializer.validated_data['content'],
+                category_id=serializer.validated_data['category_id']
+            )
+            return Response({'new_post': ActorSerializer(post_new).data}, status=201)
+        return Response(serializer.errors, status=400)
